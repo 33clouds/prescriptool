1358578ae6d5e65cb328252c600b1b48e5860c3f
Kevin Gandolfi <gandolfi.kevin@gmail.com>
2022-12-06 17:03:39 +0100

    Implement Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit::Authorization
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit? # to satisfy this verification, we need to use authorize @flat
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit? # to satisfy this verification, we need to use policy_scope(Flat)
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
+
+["index", "show", "edi"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :prescriptions, only: %i[index show]
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
