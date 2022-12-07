83e8db2067c6d1f27c02f1fdad85b6b6612e1c47
Kevin Gandolfi <gandolfi.kevin@gmail.com>
2022-12-07 19:47:09 +0100

    Played with QR code generator

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,3 +1,5 @@
+require "rqrcode"
+
 class PrescriptionsController < ApplicationController
   def index
     @prescriptions = policy_scope(Prescription)
@@ -7,5 +9,43 @@ class PrescriptionsController < ApplicationController
   def show
     @prescription = Prescription.find(params[:id])
     authorize @prescription
+
+    # data_array = []
+    # @prescription.meds.each do |med|
+    #   data_array << {data: med.name, mode: :byte_8bit}
+    #   med.meds_prescriptions.each do |meds_prescription|
+    #     data_array << {data: meds_prescription.dosage, mode: :byte_8bit}
+    #   end
+    # end
+
+    createqr_code(create_string(@prescription))
+
+    # qrcode = RQRCode::QRCode.new("#{request.base_url}/prescriptions/#{@prescription.id}")
+  end
+
+  private
+
+  def create_string(prescription)
+    data_array = ""
+    data_array += "Dr #{prescription.professional.last_name}\n"
+    data_array += "Patient: #{prescription.patient.first_name} #{prescription.patient.last_name}\n"
+    prescription.meds.each do |med|
+      data_array += "#{med.name}: "
+      med.meds_prescriptions.each do |meds_prescription|
+        data_array += "#{meds_prescription.dosage}\n"
+      end
+    end
+    data_array
+  end
+
+  def createqr_code(data_array)
+    qrcode = RQRCode::QRCode.new(data_array)
+    @svg = qrcode.as_svg(
+      color: "000",
+      shape_rendering: "crispEdges",
+      module_size: 11,
+      standalone: true,
+      use_path: true
+    )
   end
 end
