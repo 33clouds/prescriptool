8e04a6190dce75deebe14f6131155add20467fc5
Rodrigo Borges <rasb140@hotmail.com>
2022-12-08 12:38:14 -0100

    progress on Showpage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :prescriptions, only: %i[index show]
 
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
