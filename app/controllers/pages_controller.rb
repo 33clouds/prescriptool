4c975c05938a0827df036a764c7f4639fdf7517f
Olga Grigoryeva <33clouds@gmail.com>
2022-12-08 16:51:03 +0100

    Navbar improvements
      * Remove stock hamburger menu;
    
      * setup default user pic;
    
      * implement redirect / -> /prescriptions, only for logged-in users;
    
      * Prescriptool icon / title / subtitle linkd to /;
    
      * set login link to blue color.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    redirect_to prescriptions_path if user_signed_in?
   end
 end
