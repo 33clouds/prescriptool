b9268d43e85883a844de3d10bb12163bdb42b46d
Olga Grigoryeva <33clouds@gmail.com>
2022-12-14 07:56:40 +0100

    Get rid of extra controllers for the  prescription list

--- app/javascript/controllers/prescription_list_controller.js
+++ app/javascript/controllers/prescription_list_controller.js
@@ -2,13 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="prescription-list"
 export default class extends Controller {
-  static targets = ["card"]
+  static targets = ["modalqr"];
 
   connect() {
     this.selectedCard = null;
   }
 
-  toggle(event) {
+  toggleItem(event) {
     const p = event.currentTarget;
 
     if (this.selectedCard)
@@ -20,5 +20,18 @@ export default class extends Controller {
       p.classList.add("expand");
       this.selectedCard = p;
     }
+  }
+
+  swallow(event) {
+    event.stopPropagation();
+  }
+
+  onModalShow(event) {
+    // https://getbootstrap.com/docs/4.0/components/modal/#varying-modal-content
+    // The triggering url has data-id (the relevant prescription id);
+    // the method executes when the modal is shown. It is registered via
+    // `show.bs.modal` event on the modal itself.
+    const id = event.relatedTarget.dataset.id;
+    this.modalqrTarget.src = `/prescriptions/${id}/qr`;
   }
 }
